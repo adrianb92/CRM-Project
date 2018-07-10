c327d2224e80e89ebe8cc202221ea401ab4ec519
adrianb92 <adrian.barczuk@gmail.com>
2018-07-10 19:30:15 +0200

    Add service layer

--- src/com/adrianbarczuk/springdemo/controller/CustomerController.java
+++ src/com/adrianbarczuk/springdemo/controller/CustomerController.java
@@ -5,22 +5,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.adrianbarczuk.springdemo.dao.CustomerDAO;
 import com.adrianbarczuk.springdemo.entity.Customer;
+import com.adrianbarczuk.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
-	@RequestMapping("/list") 
+	@GetMapping("/list") 
 	public String listCustomers(Model model) {
 		
-		List<Customer> customers = customerDAO.getCustomers();
+		List<Customer> customers = customerService.getCustomers();
 		
 		//add customers to the model
 		model.addAttribute("customers", customers);
---
+++ src/com/adrianbarczuk/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,27 @@
+package com.adrianbarczuk.springdemo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.adrianbarczuk.springdemo.dao.CustomerDAO;
+import com.adrianbarczuk.springdemo.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		List<Customer> customers = customerDAO.getCustomers();
+		
+		return customers;
+	}
+
+}
